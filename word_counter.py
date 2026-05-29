35c9795e1ecd98e2a7bad8716ebf2438ec7431e4
Sasha7543 <lisijsasa70@gmail.com>
2026-05-29 15:59:23 +0300

    Initialize project structure

---
+++ word_counter.py
@@ -0,0 +1,10 @@
+"""Utilities and CLI for finding the most common words in a text file."""
+
+
+def main():
+    """Run the command-line interface."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
